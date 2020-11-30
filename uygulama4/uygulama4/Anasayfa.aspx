53f59e64169761169557dc4387b77cfe42730071
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-11-30 23:21:32 +0300

    smtp ile eposta gonderme

---
+++ uygulama4/uygulama4/Anasayfa.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Anasayfa.aspx.cs" Inherits="uygulama4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Kime :"></asp:Label>
+            <asp:TextBox ID="txtKime" runat="server" Width="218px"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Konu :"></asp:Label>
+            <asp:TextBox ID="txtKonu" runat="server" Width="218px"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="İçerik :"></asp:Label>
+            <asp:TextBox ID="txtIcerik" runat="server" Height="82px" TextMode="MultiLine" Width="293px"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnGonder" runat="server" OnClick="btnGonder_Click" Text="Gönder" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ uygulama4/uygulama4/Anasayfa.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+
+namespace uygulama4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGonder_Click(object sender, EventArgs e)
+        {
+            MailMessage ePosta = new MailMessage();
+            ePosta.From = new MailAddress("htcnrnlbnt@gmail.com"); /* e postayı kimden göndereceğimizi yazıyoruz.*/
+            ePosta.To.Add(txtKime.Text); /* birden fazla kişiye mail göndereceksek bu satır bir kaç kez kopyalanıp yapıştırılır. */
+            ePosta.Subject = txtKonu.Text;
+            ePosta.Body = txtIcerik.Text;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("ornek@gmail.com", "123456"); /* mail adres ve şifresini giriniz.*/ 
+            smtp.Port = 587;
+            smtp.Host = "mail.google.com";
+            smtp.Send(ePosta);
+            btnGonder.Text = "e-posta başarıyla gönderildi !";
+        }
+    }
+}
