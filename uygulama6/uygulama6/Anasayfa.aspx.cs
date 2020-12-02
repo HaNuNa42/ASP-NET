c72d81dcdeea9c15efb2aed3cbd750ec39fc1417
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-02 16:40:45 +0300

    uyarı penceresi oluşturma

---
+++ uygulama6/uygulama6/Anasayfa.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama6
+{
+    public partial class Anasayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<script lang='JavaScript'>alert('Uyarı Mesajı!');</script>");
+        }
+    }
+}
