e93c4a8919855a8aa0ac4de7ebd043d294a4a686
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-02 16:09:35 +0300

    form üzerine html kodu ekleme

---
+++ uygulama5/uygulama5/Anasayfa.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<H1><b>Merhaba Dünya! <b/><H1>");
+        }
+    }
+}
