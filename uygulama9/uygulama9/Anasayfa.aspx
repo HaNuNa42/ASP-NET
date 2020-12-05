c08dec3400d09a90106aece42f511336ba0c97c0
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-05 16:17:43 +0300

    metod çağırma

---
+++ uygulama9/uygulama9/Anasayfa.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Anasayfa.aspx.cs" Inherits="uygulama9.Anasayfa" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Getir" Width="153px" />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ uygulama9/uygulama9/Anasayfa.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Runtime.CompilerServices;
+
+namespace uygulama9
+{
+    public partial class Anasayfa : System.Web.UI.Page
+    {
+        protected void IsimOlustur(string ad, string soyad, [CallerFilePath] string dosya = "",
+            
+            [CallerMemberName] string metod = "",
+            [CallerLineNumber] int satir = -1)
+        {
+            string isim = ad + " " + soyad;
+            Label1.Text = "kişinin ismi: " + isim + "<br/>" +
+                "<b>Metodu çağıran metodun bilgileri</b><br/>" +
+                "- Dosya: " + dosya + "<br/>" +
+                "- Metod: " + metod + "<br/>" +
+                "- Satır: " + satir;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            IsimOlustur("hatice", "nur");
+        }
+    }
+}
