c685d0491ee204c732e7fe9348e59f416e02b1b1
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-09 18:09:14 +0300

    ImageButton

---
+++ uygulama22/uygulama22/KodlaEkle.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="KodlaEkle.aspx.cs" Inherits="uygulama22.KodlaEkle" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ImageButton ID="ImageButton1" runat="server" OnClick="ImageButton1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ uygulama22/uygulama22/KodlaEkle.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama22
+{
+    public partial class KodlaEkle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ImageButton1.ImageUrl = "yazilim.jpg";
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("https://haticenurnalbant.com");
+        }
+    }
+}
