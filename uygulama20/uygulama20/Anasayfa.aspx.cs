93b67d7423c9aaf3afd311da13d51dece4ed8081
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-09 17:10:53 +0300

    HyperLink

---
+++ uygulama20/uygulama20/Anasayfa.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama20
+{
+    public partial class Anasayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HyperLink1.NavigateUrl = "https://haticenurnalbant.com";
+            HyperLink1.Text = "Hatice Nur";
+            HyperLink1.ForeColor = System.Drawing.Color.Pink;
+        }
+    }
+}
