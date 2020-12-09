b555f24de511f683f7aa8a6e52c020df7a84facb
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-09 16:27:44 +0300

    FileUpload ve özellikleri

---
+++ uygulama18/uygulama18/Anasayfa.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama18
+{
+    public partial class Anasayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile) /** hasfile ile dosya seçilip seçilmediği bilgisini alıyoruz. **/
+            {
+                string dosyaAdi = System.IO.Path.GetFileName(FileUpload1.FileName);
+                FileUpload1.SaveAs(Server.MapPath(dosyaAdi)); /** server.mathpath: dosyayı nereye yükleyeceğimiz bilgisini alıyoruz. bu mettod ile dosya sunucu içerisindeki dosyaların olduğu ana klasöre yüklenmektedir.**/
+            }
+        }
+    }
+}
---
+++ uygulama18/uygulama18/DosyaYukleme2.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama18
+{
+    public partial class DosyaYukleme2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
