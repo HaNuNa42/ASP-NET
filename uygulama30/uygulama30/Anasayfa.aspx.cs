518698559ff9869b2dbca26db656b2562e3b5565
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-09 23:47:26 +0300

    PlaceHolder (dinamik kontrol oluşturma) kullanımı

---
+++ uygulama30/uygulama30/Anasayfa.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama30
+{
+    public partial class Anasayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label lblAd = new Label();
+            lblAd.ID = "lblAd";
+            lblAd.Text = "hatice nur";
+            PlaceHolder1.Controls.Add(lblAd);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label lblAd = PlaceHolder1.FindControl("lblAd") as Label; /** placeholder'ın kontrole erişimi için findcontrol metodu kullanılır.**/
+            Label1.Text = lblAd.Text;
+        }
+    }
+}
