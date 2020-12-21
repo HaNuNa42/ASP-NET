34817b81a250a2569b45b7f9f59745a61f5a7d40
Hatice Nur Nalbant <58957774+HaNuNa42@users.noreply.github.com>
2020-12-21 17:04:07 +0300

    table kullanımı

---
+++ uygulama34/uygulama34/Anasayfa.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Anasayfa.aspx.cs" Inherits="uygulama34.Anasayfa" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Table ID="Table1" runat="server">
+            </asp:Table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ uygulama34/uygulama34/Anasayfa.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uygulama34
+{
+    public partial class Anasayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int sutunSayisi = 3;
+            int satirSayisi = 5;
+
+            for (int i = 0; i<satirSayisi; i++)
+            {
+                TableRow satir = new TableRow();
+                for(int j=0; j <sutunSayisi; j++)
+                {
+                    TableCell sutun = new TableCell();
+                    sutun.Text = "Sütun : " + i + "-" + j;
+                    sutun.BorderStyle = BorderStyle.Solid;
+
+                    satir.Cells.Add(sutun);
+                }
+                Table1.Rows.Add(satir);
+            }
+        }
+    }
+}
